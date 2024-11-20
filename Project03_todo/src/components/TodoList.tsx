941ccffae4b14a3a91f160e6a21cb0dd5d2a5bfa
Sailesh Gurung <saileshgurung98@gmail.com>
2024-11-20 10:43:30 +0545

    update

--- Project03_todo/src/components/TodoList.tsx
+++ Project03_todo/src/components/TodoList.tsx
@@ -1,5 +1,26 @@
+import DeleteButton from "./DeleteButton";
+
+const initialTodos = [
+    "buy groceries",
+    "walk the dog",
+    "do laundry",
+    "study for exam"
+];
+
 export default function TodoList () {
     return(
-        <ul></ul>
+        <ul>
+            {
+                initialTodos.map((todo)=>(
+                    <li 
+                        key={todo} 
+                        className="flex justify-between items-center px-8 h-[50px] text-[14px] cursor-pointer border-b border-black/[8%]"
+                    >
+                        <span>{todo}</span>
+                        <DeleteButton />
+                    </li>
+                ))
+            }
+        </ul>
     )
 }
