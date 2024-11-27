4f6acec3ff35248529640212f55ae673659d22ab
Sailesh Gurung <saileshgurung98@gmail.com>
2024-11-27 22:16:50 +0545

    update

--- Project03_todo/src/main.tsx
+++ Project03_todo/src/main.tsx
@@ -10,8 +10,21 @@ createRoot(document.getElementById('root')!).render(
     <KindeProvider
       clientId="9fe4207c0c244eb0aa3e434c51a5b356"
       domain="https://selfbusiness.kinde.com"
-      redirectUri="http://localhost:5173"
-      logoutUri="http://localhost:5173"
+      redirectUri={
+        // conditional expression that checks the value of process.env.NODE_ENV to determine which URL to use, based on whether the application is running in a production or development environment.
+        process.env.NODE_ENV === "production"
+        ? "http://localhost:5173"
+        : "https://react-project-jade-mu.vercel.app/"
+      }
+        
+      logoutUri={
+        process.env.NODE_ENV === "production"
+        ? "http://localhost:5173"
+        : "https://react-project-jade-mu.vercel.app/"
+      }
+      // used to conditionally enable or disable the use of localStorage in a React component
+      isDangerouslyUseLocalStorage = {true}
+
 	  >
       <TodosContextProvider >
         <App />
