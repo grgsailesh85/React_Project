c7f893752e130d2f547792bd44f46079f0ef539c
Sailesh Gurung <saileshgurung98@gmail.com>
2024-11-29 13:45:34 +0545

    update

---
+++ Project04_crud/src/App.jsx
@@ -0,0 +1,10 @@
+// import "../src/App.css"
+import EmployeeList from "./components/EmployeeList";
+const App =()=>{
+  return (
+    <div className="App">
+      <EmployeeList/>
+    </div>
+  )
+}
+export default App;
