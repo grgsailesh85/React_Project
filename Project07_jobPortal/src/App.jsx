342bb34813cf84e4045d1d037f2f7a25b8332625
Sailesh Gurung <saileshgurung98@gmail.com>
2024-12-11 22:16:15 +0545

    updates

--- Project07_jobPortal/src/App.jsx
+++ Project07_jobPortal/src/App.jsx
@@ -5,12 +5,19 @@ import {
   RouterProvider
 } from 'react-router-dom'
 import Homepage from './pages/Homepage';
-
+import MainLayouts from './layouts/MainLayouts';
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const router =createBrowserRouter(
-  createRoutesFromElements(<Route index element={<Homepage/>} />)
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayouts />}>     
+      <Route index element={<Homepage/>}/>
+      <Route path='/jobs' element={<JobsPage/>}/>
+      <Route path='*' element={<NotFoundPage/>}/>
+    </Route>
+  )
 )
-
 const App = () => {
   return <RouterProvider router={router} />;
 }
--- Project07_jobPortal/src/pages/Homepage.jsx
+++ Project07_jobPortal/src/pages/Homepage.jsx
@@ -1,8 +1,16 @@
-const Homepage = () => {
-    
+import Hero from '../components/Hero'
+import HomeCards from '../components/HomeCards'
+import JobListings from '../components/JobListings'
+import ViewAllJobs from '../components/ViewAllJobs'
 
+const Homepage = () => {
     return (
-        <div>Homepage</div>
+        <>
+            <Hero/>
+            <HomeCards/>
+            <JobListings isHome={true}/>
+            <ViewAllJobs />
+        </>
     )
 }
 
