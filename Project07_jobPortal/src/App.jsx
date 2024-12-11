cab5da2dcedef5d90716725e4f45094d40da59cd
Sailesh Gurung <saileshgurung98@gmail.com>
2024-12-11 18:44:28 +0545

    update

--- Project07_jobPortal/src/App.jsx
+++ Project07_jobPortal/src/App.jsx
@@ -1,15 +1,18 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import HomeCard from "./components/HomeCard"
+import { 
+  Route, 
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom'
+import Homepage from './pages/Homepage';
+
+
+const router =createBrowserRouter(
+  createRoutesFromElements(<Route index element={<Homepage/>} />)
+)
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCard /> 
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
---
+++ Project07_jobPortal/src/components/ViewAllJobs.jsx
@@ -0,0 +1,14 @@
+const ViewAllJobs = () => {
+  return (
+    <section>
+        <a 
+            href="/jobs"
+            className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+            View All Jobs
+        </a>
+    </section>
+  )
+}
+
+export default ViewAllJobs
