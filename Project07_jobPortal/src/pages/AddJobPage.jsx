2ee7d5543af5600e0b21d7eafb3aa09bb2d3e035
Sailesh Gurung <saileshgurung98@gmail.com>
2024-12-16 20:17:21 +0545

    Update AddJobPage.jsx

--- Project07_jobPortal/src/pages/AddJobPage.jsx
+++ Project07_jobPortal/src/pages/AddJobPage.jsx
@@ -44,7 +44,7 @@ const AddJobPage = ({ addJobSubmit }) => {
       <div className='container m-auto max-w-2xl py-24'>
         <div className='bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0'>
           <form onSubmit={submitForm}>
-            <h2 className='text-3xl text-center font-semibold mb-6'>Add Job</h2>
+            <h2 className='text-4xl text-center font-semibold mb-6'>Add Job</h2>
 
             <div className='mb-4'>
               <label
