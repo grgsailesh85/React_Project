42b1e525c7a06d6f15972090a65da24fa163f4b0
Sailesh Gurung <saileshgurung98@gmail.com>
2025-02-21 15:48:30 +0545

    completed

--- contextAPItodo/src/App.jsx
+++ contextAPItodo/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TodoProvider } from "./contexts";
+import { TodoForm, TodoItem } from "./components";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
@@ -7,6 +8,41 @@ const App = () => {
   const addTodo = (todo) => {
     setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
   };
+
+  // This code defines a function updateTodo that takes an id and a todo object. It updates a list of todos (setTodos) by mapping through the previous list (prev). If a todo's id matches the given id, it replaces that todo with the new todo object. Otherwise, it keeps the original todo unchanged. In short, it updates a specific todo in the list.
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  //The toggleComplete function toggles the completed status of a todo with a matching id. It updates the list by flipping the completed value (from true to false or vice versa) for the matching todo while keeping the rest unchanged. In short, it switches the completion status of a specific todo.
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
+
+  useEffect(() => {
+    // JSON.parse(...) – Converts the JSON string into a JavaScript object or array.
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    // JSON.stringify(todos) – Converts the JavaScript object/array into a JSON string.
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <TodoProvider
@@ -17,9 +53,16 @@ const App = () => {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4">
+            <TodoForm />
+          </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo}/>
+              </div>
+            ))}
           </div>
         </div>
       </div>
